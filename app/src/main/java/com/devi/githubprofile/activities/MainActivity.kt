37976c2b67afa40386268adaf09fb42eccafc7aa
RPL-10 <deviyulianty64@gmail.com>
2022-06-23 09:17:43 +0700

    GithubProfile Mvvm

--- app/src/main/java/com/devi/githubprofile/activities/MainActivity.kt
+++ app/src/main/java/com/devi/githubprofile/activities/MainActivity.kt
@@ -18,9 +18,9 @@ import retrofit2.Call
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    companion object {
-        val TAG: String = MainActivity::class.java.simpleName
-    }
+//    companion object {
+//        val TAG: String = MainActivity::class.java.simpleName
+//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -35,44 +35,44 @@ class MainActivity : AppCompatActivity() {
             showLoading(it)
         }
 
-
-
         binding.btnSearchUserLogin.setOnClickListener {
             val userLogin = binding.etSearchUserLogin.text.toString()
+            var query = Config.DEFAULT_USER_LOGIN
             if (userLogin.isNotEmpty()) {
-                searchUser(userLogin)
+                query=userLogin
             }
+           mainViewModel.searchUser(query)
         }
-        searchUser(Config.DEFAULT_USER_LOGIN)
+
     }
-    private fun searchUser(query: String){
-        showLoading(true)
-        Log.d(TAG, "getDataUserProfileFromAPI: start...")
-        val githubUserService: GithubUserService = ServiceBuilder.buildService(GithubUserService::class.java)
-        val requestCall: Call<GithubUser> = githubUserService.loginUser(query)
-        requestCall.enqueue(object : retrofit2.Callback<GithubUser> {
-            override fun onResponse(call: Call<GithubUser>, response: retrofit2.Response<GithubUser>) {
-                showLoading(false)
-                if(response.isSuccessful){
-                    val result = response.body()
-                    if (result != null) {
-                        setUserData(result)
-                    }
-                    Log.d(TAG, "getDataUserFromAPI: onResponse finish...")
-                }else{
-                    binding.tvUser.text = "User Not Found"
-                    GlideApp.with(applicationContext)
-                        .load(R.drawable.ic_baseline_broken_image_24)
-                        .into(binding.imgUser)
-                    Log.d(TAG, "getDataUserFromAPI: onResponse failed...")
-                }
-            }
-            override fun onFailure(call: Call<GithubUser>, t: Throwable) {
-                showLoading(false)
-                Log.d(TAG, "getDataUserFromAPI: onFailure ${t.message}...")
-            }
-        })
-    }
+//    private fun searchUser(query: String){
+//        showLoading(true)
+//        Log.d(TAG, "getDataUserProfileFromAPI: start...")
+//        val githubUserService: GithubUserService = ServiceBuilder.buildService(GithubUserService::class.java)
+//        val requestCall: Call<GithubUser> = githubUserService.loginUser(query)
+//        requestCall.enqueue(object : retrofit2.Callback<GithubUser> {
+//            override fun onResponse(call: Call<GithubUser>, response: retrofit2.Response<GithubUser>) {
+//                showLoading(false)
+//                if(response.isSuccessful){
+//                    val result = response.body()
+//                    if (result != null) {
+//                        setUserData(result)
+//                    }
+//                    Log.d(TAG, "getDataUserFromAPI: onResponse finish...")
+//                }else{
+//                    binding.tvUser.text = "User Not Found"
+//                    GlideApp.with(applicationContext)
+//                        .load(R.drawable.ic_baseline_broken_image_24)
+//                        .into(binding.imgUser)
+//                    Log.d(TAG, "getDataUserFromAPI: onResponse failed...")
+//                }
+//            }
+//            override fun onFailure(call: Call<GithubUser>, t: Throwable) {
+//                showLoading(false)
+//                Log.d(TAG, "getDataUserFromAPI: onFailure ${t.message}...")
+//            }
+//        })
+//    }
     private fun setUserData(githubUser: GithubUser) {
         binding.tvUser.text = githubUser.toString()
         GlideApp.with(applicationContext)
